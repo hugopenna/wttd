adb1e11052ce068e894d2c1427633146a05bcd3b
Hugo Penna <10089299+hugopenna@users.noreply.github.com>
2022-05-10 22:40:28 -0300

    Fixing the uuid migration as primary key - Try 4

--- eventex/subscriptions/migrations/0005_auto_20220511_0107.py
+++ eventex/subscriptions/migrations/0005_auto_20220511_0107.py
@@ -11,10 +11,6 @@ class Migration(migrations.Migration):
     ]
 
     operations = {
-        migrations.RemoveField(
-            model_name='subscription',
-            name='id',
-        ),
         migrations.AlterField(
             model_name='subscription',
             name='temp_id',
