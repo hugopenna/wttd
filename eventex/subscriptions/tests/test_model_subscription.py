060608502749becce8ac6d3795d3ddb3b1b5a5d6
Hugo Penna <git@hugopenna.com>
2022-09-13 09:08:50 -0300

    add campo pago

--- eventex/subscriptions/tests/test_model_subscription.py
+++ eventex/subscriptions/tests/test_model_subscription.py
@@ -22,3 +22,7 @@ class SubscriptionModelTest(TestCase):
 
     def test_str(self):
         self.assertEqual('Hugo Penna', str(self.obj))
+
+    def test_paid_default_to_False(self):
+        """By Default paid must be false"""
+        self.assertEqual(False, self.obj.paid)
