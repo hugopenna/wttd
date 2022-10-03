4bc42b26ffda27a74a18ec78ac0231f6d5c7bd81
Hugo Penna <git@hugopenna.com>
2022-10-03 15:08:15 -0300

    Marcar como pago no admin

--- eventex/subscriptions/tests/test_admin.py
+++ eventex/subscriptions/tests/test_admin.py
@@ -1,20 +1,39 @@
+from unittest.mock import Mock
+
 from django.test import TestCase
 from eventex.subscriptions.admin import SubscriptionModelAdmin, Subscription, admin
 
 
 class SubscriptionModelAdminClass(TestCase):
+    def setUp(self):
+        Subscription.objects.create(name='paul walker', cpf='12345678901', email='me@me.com', phone='11999991234')
+        self.model_admin = SubscriptionModelAdmin(Subscription, admin.site)
+
     def test_has_action(self):
         """Action mark_as_paid shuld be installed"""
-        model_admin = SubscriptionModelAdmin(Subscription, admin.site)
-        self.assertIn('mark_as_paid', model_admin.actions)
+        self.assertIn('mark_as_paid', self.model_admin.actions)
 
     def test_mark_all(self):
         """It should mark all selected as paid"""
-        Subscription.objects.create(name='paul walker', cpf='12345678901', email='me@me.com', phone='11999991234')
-        model_admin = SubscriptionModelAdmin(Subscription, admin.site)
-
-        queryset = Subscription.objects.all()
-
-        model_admin.mark_as_paid(None, queryset)
+        self.call_action()
 
         self.assertEqual(1, Subscription.objects.filter(paid=True).count())
+
+    def test_message(self):
+        """It should send a message to the user"""
+        mock = self.call_action()
+
+        mock.assert_called_once_with(None, '1 inscricao foi marcada como paga.')
+
+    def call_action(self):
+        queryset = Subscription.objects.all()
+
+        mock = Mock()
+        old_message_user = SubscriptionModelAdmin.message_user
+        SubscriptionModelAdmin.message_user = mock
+
+        self.model_admin.mark_as_paid(None, queryset)
+
+        SubscriptionModelAdmin.message_user = old_message_user
+
+        return mock
