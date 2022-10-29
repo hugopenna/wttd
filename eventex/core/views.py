c650ef15abc4e9120a809e9aaef0c148ed1facc6
Hugo Penna <git@hugopenna.com>
2022-10-29 12:58:31 -0300

    WIP Talk List

--- eventex/core/views.py
+++ eventex/core/views.py
@@ -10,3 +10,6 @@ def home(request):
 def speaker_detail(request, slug):
     speaker = get_object_or_404(Speakers, slug=slug)
     return render(request, 'core/speaker_detail.html', {'speaker': speaker})
+
+def talk_list(request):
+    return render(request, 'core/talk_list.html')
