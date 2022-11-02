e5f42719f3707450e65766fdd26478a5c81593a0
Hugo Penna <git@hugopenna.com>
2022-11-01 21:47:55 -0300

    WIP Talk List

--- eventex/core/tests/test_model_talk.py
+++ eventex/core/tests/test_model_talk.py
@@ -12,3 +12,12 @@ class TalkModelTest(TestCase):
 
     def test_create(self):
         self.assertTrue(Talk.objects.exists())
+
+    def test_has_speaker(self):
+        """Talk has many speakers and vice-versa"""
+        self.talk.speakers.create(
+            name='Grace Hopper',
+            slug='grace-hopper',
+            website='http://hbn.link/hopper-site'
+        )
+        self.assertEqual(1, self.talk.speakers.count())
