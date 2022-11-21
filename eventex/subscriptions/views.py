8d8088dfd6000b7775b2bc8d6c4f6affabc2a085
Hugo Penna <git@hugopenna.com>
2022-11-21 10:23:11 -0300

    WIP - CBV usando poderes do django

--- eventex/subscriptions/views.py
+++ eventex/subscriptions/views.py
@@ -1,59 +1,33 @@
 from django.conf import settings
 from django.core import mail
-from django.http import HttpResponseRedirect
-from django.shortcuts import render, resolve_url as r
 from django.template.loader import render_to_string
-from django.views import View
 from django.views.generic import DetailView
-from django.views.generic.base import TemplateResponseMixin
+from django.views.generic.edit import CreateView
 
 from eventex.subscriptions.forms import SubscriptionForm
 from eventex.subscriptions.models import Subscription
 
 
-class SubscriptionCreate(TemplateResponseMixin, View):
-    template_name = 'subscriptions/subscription_form.html'
+class SubscriptionCreate(CreateView):
+    model = Subscription
     form_class = SubscriptionForm
 
-    def get(self, *args, **kwargs):
-        return self.render_to_response(self.get_context_data())
-
-    def post(self, *args, **kwargs):
-        form = self.get_form()
-
-        if not form.is_valid():
-            return self.form_invalid(form)
-        return self.form_valid(form)
-
-    def form_invalid(self, form):
-        return self.render_to_response(self.get_context_data(form=form))
-
     def form_valid(self, form):
-        subscription = form.save()
+        response = super().form_valid(form)
+        self.send_mail()
 
-        _send_mail('Confirmação de inscrição',
-                   settings.DEFAULT_FROM_EMAIL,
-                   subscription.email,
-                   'subscriptions/subscription_email.txt',
-                   {'subscription': subscription})
+        return response
 
-        return HttpResponseRedirect(r('subscriptions:detail', subscription.pk))
+    def send_mail(self):
+        subject = 'Confirmação de inscrição'
+        from_ = settings.DEFAULT_FROM_EMAIL
+        to = self.object.email
+        template_name = 'subscriptions/subscription_email.txt'
+        context = {'subscription': self.object}
 
-    def get_form(self):
-        if self.request.method == 'POST':
-            return self.form_class(self.request.POST)
-        return self.form_class()
-
-    def get_context_data(self, **kwargs):
-        context = dict(**kwargs)
-        context.setdefault('form', self.get_form())
-        return context
+        body = render_to_string(template_name, context)
+        mail.send_mail(subject, body, from_, [from_, to])
 
 new = SubscriptionCreate.as_view()
 
 detail = DetailView.as_view(model=Subscription)
-
-
-def _send_mail(subject, from_, to, template_name, context):
-    body = render_to_string(template_name, context)
-    mail.send_mail(subject, body, from_, [from_, to])
