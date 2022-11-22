76c50bcd539c30c31f20e6eb99427be10aa3ddb7
Hugo Penna <git@hugopenna.com>
2022-11-22 08:25:23 -0300

    CBV e mixins

--- eventex/subscriptions/views.py
+++ eventex/subscriptions/views.py
@@ -1,33 +1,11 @@
-from django.conf import settings
-from django.core import mail
-from django.template.loader import render_to_string
 from django.views.generic import DetailView
-from django.views.generic.edit import CreateView
 
 from eventex.subscriptions.forms import SubscriptionForm
+from eventex.subscriptions.mixins import EmailCreateView
 from eventex.subscriptions.models import Subscription
 
-
-class SubscriptionCreate(CreateView):
-    model = Subscription
-    form_class = SubscriptionForm
-
-    def form_valid(self, form):
-        response = super().form_valid(form)
-        self.send_mail()
-
-        return response
-
-    def send_mail(self):
-        subject = 'Confirmação de inscrição'
-        from_ = settings.DEFAULT_FROM_EMAIL
-        to = self.object.email
-        template_name = 'subscriptions/subscription_email.txt'
-        context = {'subscription': self.object}
-
-        body = render_to_string(template_name, context)
-        mail.send_mail(subject, body, from_, [from_, to])
-
-new = SubscriptionCreate.as_view()
+new = EmailCreateView.as_view(model=Subscription,
+                              form_class=SubscriptionForm,
+                              email_subject='Confirmação de inscrição')
 
 detail = DetailView.as_view(model=Subscription)
